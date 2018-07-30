b76d1edd4fdf9aefe6d8666e5fa23749dff919a1
DavidWells <hello@davidwells.io>
2018-07-30 10:11:08 -0700

    style details

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -4,59 +4,59 @@ import { movieDetails, getShowtimes } from '../../utils/api'
 
 export default class Details extends Component {
   state = {
-      movie : {
-        actors: []
-      },
-      showtimes: []
+    movie: {
+      actors: []
+    },
+    showtimes: []
   }
 
-  componentDidMount(){
+  componentDidMount() {
     const id = this.props.location.pathname.replace('/details/', '')
-    movieDetails(id).then(data=>{
+    movieDetails(id).then(data => {
       this.setState({
-          movie: data
+        movie: data
       })
-    });
-    
-    getShowtimes().then(data=>{
+    })
+
+    getShowtimes().then(data => {
       this.setState({
         showtimes: data.data
       })
     })
   }
 
-  seeMovie(showtime){
+  seeMovie(showtime) {
     alert(`You have chosen the ${showtime} showtime. Enjoy the show!`)
   }
 
-  renderActors(){
+  renderActors() {
     const actors = this.state.movie.actors
     return actors.map((actor, i) => {
       return (
-        <div key={i}>
+        <div key={i} className='actor'>
           <strong>{actor}</strong>
         </div>
       )
     })
   }
 
-  renderShowtimes(){
+  renderShowtimes() {
     const showtimes = this.state.showtimes
     return showtimes.map((showtime, i) => {
       return (
         <div key={i} className="col-sm-4 col-lg-3 showtime">
-          <a className="btn btn-light btn-block" onClick={()=>this.seeMovie(showtime)}>
+          <a className="btn btn-light btn-block" onClick={() => this.seeMovie(showtime)}>
             {showtime}
           </a>
         </div>
       )
     })
   }
-  
+
   render() {
-    const movie = this.state.movie;
+    const movie = this.state.movie
     const style = {
-        backgroundColor: movie.color
+      backgroundColor: movie.color
     }
     const isAuthed = this.props.isAuthed
     return (
@@ -64,15 +64,17 @@ export default class Details extends Component {
         <div className="container">
           <div className="row movie-details">
             <div className="col-sm-3 movie-picture" style={style}>
-                <img src={movie.image} style={{width:'100%'}} alt="Poster" />
+              <img src={movie.image} style={{width: '100%'}} alt="Poster" />
             </div>
             <div className="col-sm-9 movie-info card">
-                <h1>{movie.title}</h1>
-                <h3>Starring:</h3>
+              <h1>{movie.title}</h1>
+              <h3>Starring:</h3>
+              <div className="actors">
                 {this.renderActors()}
+              </div>
             </div>
           </div>
-          <div className="row">
+          <div className="row showtimes">
             <div className="col-sm-12">
               <h2>Showtimes</h2>
               <div className="row">
